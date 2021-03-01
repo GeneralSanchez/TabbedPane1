b0470da02f09f3098de816b3b9e7d412b725fbee
Andy Sanchez <sanchezzandy206@gmail.com>
2021-03-01 19:46:32 +0000

    Tabs and buttons added

--- JTabbedPaneFrame.java
+++ JTabbedPaneFrame.java
@@ -1,9 +1,45 @@
 import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
 
-public class JTabbedPaneFrame extends JFrame 
+public class JTabbedPaneFrame extends JFrame
 {
   public JTabbedPaneFrame()
   {
     super("Andy's JTabbedPane Demo");
+
+    //create the tabbed pane  hold tab formatting
+    JTabbedPane tabPane = new JTabbedPane();
+
+    //add first tab
+    JLabel label1 = new JLabel ("This is the big area called panel one", SwingConstants.CENTER);
+    JPanel panel1 = new JPanel();
+    panel1.add(label1);
+    tabPane.addTab("Tab One", null, panel1, "FirstPanel");
+
+    //add second tab
+    JLabel label2 = new JLabel ("You can add other stuff to this tabbed panel", SwingConstants.CENTER);
+    JPanel panel2 = new JPanel();
+    panel2.add(label2);
+    tabPane.addTab("Tab Too", null, panel2, "SecondPanel");
+
+    //add third tab
+    JLabel label3 = new JLabel ("Let's do something new here", SwingConstants.CENTER);
+    JPanel panel3 = new JPanel();
+    panel3.add(label3);
+    panel3.add(new JButton("North"));
+    panel3.add(new JButton("South"));
+    tabPane.addTab("Tab Three", null, panel3, "ThirdPanel");//keep the add below all the tab's formatting
+
+
+
+
+    //keep this at the bottom
+    add(tabPane);
+
   }//end default and only constructor
-}//end Frame
+
+}//end class
